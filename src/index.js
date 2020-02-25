18fc1be1613b195b1e12d2440ee11e580ce09436
Thiago Diniz <thiagoodiniz@hotmail.com>
2020-02-25 19:24:57 -0300

    using cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const cors = require('cors');
+
 const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 require('./controllers/listController')(app);
 require('./controllers/productController')(app);
+
+const port = '3002';
+
+console.log(`Server is listening on port ${ port }`);
 
 app.listen(3002);
